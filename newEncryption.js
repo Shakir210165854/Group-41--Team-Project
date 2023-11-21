bf26dc05123dbce7476a831c36a18ace03ffcf02
Shakir210165854 <148438623+Shakir210165854@users.noreply.github.com>
2023-11-21 21:48:19 +0000

    Add files via upload
    Updated SHA-256 encryption

---
+++ newEncryption.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+async function hashPassword(password) {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(password);
+    const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashedPassword = hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('');
+    console.log(hashedPassword);
+}
