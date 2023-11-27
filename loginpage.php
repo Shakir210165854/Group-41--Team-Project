0e912c4e6e50a213a97a5429dbc09bcef311b1b1
Shakir Mahmood <210165854@aston.ac.uk>
2023-11-27 15:03:34 +0000

    added signup page as well as a toggle to switch between both forms

--- loginpage.php
+++ loginpage.php
@@ -1,32 +1,73 @@
 <?php include('db_connection.php') ?>
 
 <!DOCTYPE html>
-
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+
 <head>
     <meta charset="utf-8" />
-    <title>Login </title>
+    <title>Login</title>
     <link rel="stylesheet" type="text/css" href="designForms.css" />
+    <style>
+        #signupForm{
+            display:none;
+        }
+    </style>
 </head>
+
 <body>
 
     <div class="container">
         <section id='loginForm'>
             <h1>Login</h1>
-          <!-- <form onsubmit="hashPassword(document.getElementById('password').value); return false;"> -->
             <form method="post" action="loginpage.php">
                 <label for="email">Username:</label>
-                <input type="text" id="username" name="email" required>
+                <input type="text" id="username" name="email" required />
 
                 <label for="password">Password:</label>
-                <input type="password" id="password" name="password" required>
+                <input type="password" id="password" name="password" required />
 
-                <button  type="submit" name="log-in">Login</button>
+                <button type="submit" name="log-in">Login</button>
             </form>
+
+            <p>
+                Don't have an account? <a href="#" id="showSignup">Sign up</a>
+            </p>
+        </section>
+
+        <section id='signupForm'>
+            <h1>Sign Up</h1>
+            <form method="post" action="loginpage.php">
+                <label for="email">Email:</label>
+                <input type="email" id="signupEmail" name="email" required/>
+
+                <label for="password">Password:</label>
+                <input type="password" id="signupPassword" name="password" required />
+
+                <label for="confPassword">Re-enter Password:</label>
+                <input type="Password" id="confPassword" name="confPassword" required />
+
+
+                <button type="submit" name="sign-up">Sign Up</button>
+            </form>
+
+            <p>
+                Already have an account? <a href="#" id="showLogin">Login</a>
+            </p>
         </section>
     </div>
 
+    <script>
+            document.getElementById('showSignup').addEventListener('click', function () {
+            document.getElementById('loginForm').style.display = 'none';
+            document.getElementById('signupForm').style.display = 'block';
+        });
 
+        document.getElementById('showLogin').addEventListener('click', function () {
+            document.getElementById('loginForm').style.display = 'block';
+            document.getElementById('signupForm').style.display = 'none';
+        });
+    </script>
 
 </body>
+
 </html>
