317e7d35586f1a65c78b7f703c2b52c7ec21a26c
Mehran Raja <220199384@aston.ac.uk>
2023-12-05 16:51:44 +0000

    created about us and home page

--- About_Us.php
+++ About_Us.php
@@ -3,11 +3,156 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" type="text/css" href="Design.css"/>
+    <title>About Us</title>
+    <style>
+
+body {
+    display: flex;
+    margin: 0;
+}
+
+nav {
+    flex: 1;
+    max-width: 200px;
+    display: flex;
+    justify-content: space-around;
+    flex-direction: column;
+    background: #5e1698;
+    z-index: 3; 
+}
+
+nav button {
+    background-color: #5e1698;
+    border: none;
+    color: white;
+    padding: 30px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    border-top-left-radius: 25px;
+    border-bottom-left-radius: 25px;
+}
+
+nav button:hover {
+    background-color: #252322;
+}
+
+.logo {
+    text-align: center;
+    outline: none;
+}
+
+.logo img {
+    max-height: 80px;
+}
+
+.header {
+    background-color: #5e1698;
+    color: rgb(246, 242, 242);
+    padding: 10px;
+    text-align: center;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+}
+
+.mainContent {
+    flex: 3;
+    background: #252322;
+    min-height: 100vh;
+    font-family: Montserrat, sans-serif;
+    position: relative;
+    z-index: 1; 
+}
+
+.footer {
+    background-color: #5e1698;
+    color: rgb(246, 242, 242);
+    padding: 10px;
+    text-align: center;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+}
+
+.title {
+    color: rgb(246, 242, 242);
+    font-size: 50px;
+    font-weight: bold;
+    font-family: 'Arial', sans-serif;
+    text-align: center;
+    /*background-color: #5e1698; Make the purple strip and nav bar have a gap between */
+}
+
+.subtitle {
+    color: rgb(246, 242, 242);
+    font-size: 40px;
+    font-family: 'Arial', sans-serif;
+    text-align: center;
+}
+
+.paragraph-one {
+    color: rgb(246, 242, 242);
+    font-size: 18px;
+    font-weight: bold;
+    text-align: center;
+    padding-left: 50px;
+    padding-right: 50px;
+}
+
+.paragraph-two {
+    color: rgb(246, 242, 242);
+    font-size: 16px;
+    font-style: italic;
+    text-align: center;
+    padding-left: 50px;
+    padding-right: 50px;
+}
+
+.image-container {
+    text-align: center;
+    margin-top: 20px;
+}
+
+.image-container img {
+    max-width: 100%;
+    border-radius: 10px;
+}
+
+.team-members {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+
+.team-member {
+    text-align: center;
+    margin: 20px;
+    flex: 1 0 20%;
+    max-width: 300px;
+}
+
+.team-member img {
+    max-width: 100%;
+    border-radius: 50%;
+}
+
+.team-member h2 {
+    margin-top: 10px;
+    color: #333;
+}
+
+.team-member p {
+    margin-top: 5px;
+    color: #555;
+}
+
+</style>
 </head>
 
 <body>
+
     <nav>
     <a class="logo" onclick="return false;"><img src="https://i.ibb.co/ZBsn56k/ATlogo.jpg" alt="ATlogo"></a>
         <button onclick="window.location.href = 'newHome.php';">Home</button>
@@ -17,14 +162,70 @@
     </nav>
 
     <div class='mainContent' style="color: aliceblue;">
-        <h2> About Us Page</h2>
-       
+    <h1 class="title">OUR STORY</h1>
+
+    <p class="paragraph-one">Our journey began with a simple idea—to provide computer enthusiasts and professionals with high-quality, cutting-edge components that elevate their computing experience.</p>
+
+    <div class="image-container">
+            <img src="https://www.shutterstock.com/image-vector/computer-parts-hands-repair-pc-600nw-2192555921.jpg" alt="Sample Image">
     </div>
 
-    <footer class="footer">
-        <p1>hello</p1>
-     </footer>
+    <p class="paragraph-two">Founded by a team of dedicated tech enthusiasts, we set out to create a platform that not only offers a vast selection of computer parts but also ensures that each product meets our rigorous standards for performance and reliability. Our commitment to excellence drives every aspect of our business, from the carefully curated products in our inventory to the seamless shopping experience we provide. At AlphaTech, we understand that each computer build is a unique expression of creativity and functionality. Whether you're a seasoned gamer pushing the limits of performance or a professional seeking the latest innovations for your workstations, we've got you covered.</p>
 
+    <h1 class="subtitle">Our Founders</h1>
+    
+    <div class="team-members">
+        <div class="team-member">
+            <img src="https://via.placeholder.com/150" alt="Person 1">
+            <h2>Ahmed Abdiqadir</h2>
+            <p>Founder</p>
+        </div>
+
+        <div class="team-member">
+            <img src="https://via.placeholder.com/150" alt="Person 2">
+            <h2>Amgad Salim</h2>
+            <p>Founder</p>
+        </div>
+
+        <div class="team-member">
+            <img src="https://via.placeholder.com/150" alt="Person 3">
+            <h2>Jack Brookes</h2>
+            <p>Founder</p>
+        </div>
+
+        <div class="team-member">
+            <img src="https://via.placeholder.com/150" alt="Person 4">
+            <h2>Maxwell Ansah</h2>
+            <p>Founder</p>
+        </div>
+
+
+        <div class="team-member">
+            <img src="https://via.placeholder.com/150" alt="Person 5">
+            <h2>Mehran Raja</h2>
+            <p>Founder</p>
+        </div>
+
+        <div class="team-member">
+            <img src="https://via.placeholder.com/150" alt="Person 6">
+            <h2>Mohammed Khan</h2>
+            <p>Founder</p>
+        </div>
+
+        <div class="team-member">
+            <img src="https://via.placeholder.com/150" alt="Person 7">
+            <h2>Shakir Mahmood</h2>
+            <p>Founder</p>
+        </div>
+
+        <div class="team-member">
+            <img src="https://via.placeholder.com/150" alt="Person 8">
+            <h2>Vrutik Gohel</h2>
+            <p>Founder</p>
+        </div>
+    </div>
+
+    </div>
     
 </body>
 </html>
--- newHome.php
+++ newHome.php
@@ -1,10 +1,149 @@
 ﻿<!DOCTYPE html>
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <meta charset="utf-8" />
-    <title>AlphaTech</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
     <link rel="stylesheet" type="text/css" href="Design.css"/>
 </head>
+
+<style>
+body {
+    display: flex;
+    margin: 0;
+}
+
+nav {
+    flex: 1;
+    max-width: 200px;
+    display: flex;
+    justify-content: space-around;
+    flex-direction: column;
+    background: #5e1698;
+    z-index: 3; 
+}
+
+nav button {
+    background-color: #5e1698;
+    border: none;
+    color: white;
+    padding: 30px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    border-top-left-radius: 25px;
+    border-bottom-left-radius: 25px;
+}
+
+nav button:hover {
+    background-color: #252322;
+}
+
+nav button:active {
+    background-color: #252322;
+}
+
+.logo {
+    text-align: center;
+    outline: none;
+}
+
+.logo img {
+    max-height: 80px;
+}
+
+.header {
+    background-color: #5e1698;
+    color: rgb(246, 242, 242);
+    padding: 10px;
+    text-align: center;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+}
+
+.mainContent {
+    flex: 3;
+    background: #252322;
+    min-height: 100vh;
+    font-family: Montserrat, sans-serif;
+    position: relative;
+    z-index: 1; 
+}
+
+.footer {
+    background-color: #5e1698;
+    color: rgb(246, 242, 242);
+    padding: 10px;
+    text-align: center;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+}
+
+.title {
+    color: rgb(246, 242, 242);
+    font-size: 50px;
+    font-weight: bold;
+    font-family: 'Arial', sans-serif;
+    text-align: center;
+    background-color: #5e1698;
+}
+
+.subtitle {
+    color: rgb(246, 242, 242);
+    font-size: 40px;
+    font-family: 'Arial', sans-serif;
+    text-align: center;
+}
+
+.hero-image {
+    width: 100%;
+    max-height: 400px;
+    object-fit: cover;
+    margin-bottom: 20px;
+}
+
+.paragraph-one {
+    color: rgb(246, 242, 242);
+    font-size: 18px;
+    font-weight: bold;
+    text-align: center;
+    padding-left: 50px;
+    padding-right: 50px;
+}
+
+.paragraph-two {
+    color: rgb(246, 242, 242);
+    font-size: 16px;
+    font-style: italic;
+    text-align: center;
+    padding-left: 50px;
+    padding-right: 50px;
+}
+
+login button {
+    background-color: #5e1698; /* Purple */
+    border: none;
+    color: white;
+    padding: 10px 40px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    border-top-left-radius: 25px; /* Rounded top-left corner */
+    border-bottom-left-radius: 25px; /* Rounded bottom-left corner */
+    border-top-right-radius: 25px;
+    border-bottom-right-radius: 25px;
+    margin-left: 90%;
+    margin-top: 2%;
+}
+login button:hover {
+    background-color: #420c6d; /* Purple */
+}
+</style>
+
 <body>
 
     <nav>
@@ -19,9 +158,8 @@
 
 
     <div class='mainContent' style="color: aliceblue;">
-        <h2> Home Page</h2>
         
-<!-- a login button should disappear when a user is loged-in -->
+<!-- a login button should disappear when a user is logged-in -->
         <login>
             <button onclick="window.location.href = 'loginpage.php';">login</button>
         </login>
@@ -40,18 +178,20 @@ if ($result->num_rows > 0) {
         echo "ID: " . $row["user_id"] . " - Username: " . $row["first_name"] . " - Email: " . $row["email"] . "<br>";
     }
 } else {
-    echo "0 results";
+    echo "";
 }
 
 // Close the connection
 $conn->close();
 ?>
-       
-    </div>
-    
-    <footer class="footer">
-       <p1>hello</p1>
-    </footer>
+    <h1 class="title">AlphaTech</h1>
+    <img class="hero-image" src="https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/page/campaign/alienware/gaming/awr16-aw2723df-aw420k-aw620m-aw720h-xml-pl-gaminggetaway-front.psd?fmt=png-alpha&wid=4000&hei=2249" alt="Hero Image">
+    <p class="paragraph-one">paragraph 1 here.</p>
+    <p class="paragraph-two">paragraph 2 here.</p>
+
+    <h1 class="subtitle">Subtitle here</h1>
+
+</div>
     
 </body>
 
