27520a43dd4b6369d6fea28c588009a9ba63125d
Mehran Raja <220199384@aston.ac.uk>
2023-12-09 00:46:08 +0000

    added content to home page

--- newHome.php
+++ newHome.php
@@ -30,14 +30,58 @@
 
         
     <h1 class="title">AlphaTech</h1>
-    <img class="hero-image" src="https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/page/campaign/alienware/gaming/awr16-aw2723df-aw420k-aw620m-aw720h-xml-pl-gaminggetaway-front.psd?fmt=png-alpha&wid=4000&hei=2249" alt="Hero Image">
-    <p class="paragraph-one">paragraph 1 here.</p>
-    <p class="paragraph-two">paragraph 2 here.</p>
 
-    <h1 class="subtitle">Subtitle here</h1>
+    <div class="slideshow-container">
+    <div class="mySlides">
+        <img class="hero-image" src="https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/page/campaign/alienware/gaming/awr16-aw2723df-aw420k-aw620m-aw720h-xml-pl-gaminggetaway-front.psd?fmt=png-alpha&wid=4000&hei=2249" alt="Hero Image 1">
+    </div>
+    <div class="mySlides">
+        <img class="hero-image" src="https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/page/campaign/alienware/gaming/m18-aw3423dw-aw920k-aw720m-aw920h-xml-prestige-pursuer-front-4000x2250.jpg?fmt=jpg&wid=4000&hei=2250" alt="Hero Image 2">
+    </div>
+    <div class="mySlides">
+        <img class="hero-image" src="https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/page/campaign/alienware/gaming/x16-aw3423dw-aw420k-aw720m-aw920h-xml-experience-seeker-front-4000x2250.jpg?fmt=jpg&wid=4000&hei=2250" alt="Hero Image 3">
+    </div>
+    <div class="mySlides">
+        <img class="hero-image" src="https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/page/campaign/alienware/gaming/awr15-aw3423dw-aw510k-aw620m-aw720h-xml-enthusiast-front.jpg?fmt=jpg&wid=4000&hei=2249" alt="Hero Image 4">
+    </div>
+    </div>
+
+    <h1 class="subtitle">Looking for parts for your dream setup?</h1>
+    <p class="paragraph-one">Click below to get top-of-the-line parts for the setup of your dreams</p>
+    <p class="paragraph-two"><a href="Products.php">Click here!</a></p>
+
+    <div class="side-image-container">
+    <div class="side-by-side-images">
+        <div class="side-image">
+            <img src="https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/page/campaign/alienware/gaming/awr14-aw720m-aw920h-lifestylelaptop.jpg?fmt=jpg&wid=1080&hei=720">
+            <p class="side-image-text">Want to know who made this all possible?<br><a href="About_Us.php">Click here to find out!</a></br></p>
+        </div>
+        <div class="side-image">
+            <img src="https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/page/campaign/alienware/gaming/awr14-aw920h-girlsgame.jpg?fmt=jpg&wid=1080&hei=720" alt="Image 2">
+            <p class="side-image-text">Need support with anything?<br><a href="Contact_Us.php">Click here to get in contact!</a></br></p>
+        </div>
+    </div>
+</div>
 
 </div>
     
+<script>
+    var slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) { slideIndex = 1 }
+        slides[slideIndex - 1].style.display = "block";
+        setTimeout(showSlides, 4000);
+        }
+    </script>
+
 </body>
 
 </html>
