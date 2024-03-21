51b51f5e9f5c18b674dbd8791706554cff851717
Shakir Mahmood <210165854@aston.ac.uk>
2024-03-21 14:20:35 +0000

    added logging when user has registered or logged in.

--- db_connection.php
+++ db_connection.php
@@ -34,19 +34,19 @@ if (isset($_SESSION['user_id'])) {
 if (isset($_POST['log-in'])) {
     $username = mysqli_real_escape_string($conn, $_POST['email']);
     $password = mysqli_real_escape_string($conn, $_POST['password']);
-   
+
     if (empty($username)) {
       array_push($errors, "Username is required");
     }
     if (empty($password)) {
       array_push($errors, "Password is required");
     }
-   
+
     if (count($errors) == 0) {
       $password = md5($password);
       $query = "SELECT * FROM users WHERE email='$username' AND password='$password' ";
       $results = mysqli_query($conn, $query);
-      
+
       if (mysqli_num_rows($results) == 1) {
         $user = mysqli_fetch_assoc($results);
         $uid = $user['user_id'];
@@ -55,6 +55,12 @@ if (isset($_POST['log-in'])) {
         $_SESSION['success'] = "You are now logged in";
         $userID = $uid; // Update $userID
         header('location: newHome.php');
+
+        // Log activity to the database, give date and time as well as action.
+        $timestamp = date('Y-m-d H:i:s');
+        $action="log in";
+        $insert_query = "INSERT INTO user_logs (user_id, timestamp, action) VALUES ('$userID', '$timestamp' , '$action')";
+        mysqli_query($conn, $insert_query);
 
         if($user['admin'] == 1){
           header('location: AdminHomePage.php');
@@ -69,10 +75,6 @@ if (isset($_POST['log-in'])) {
   }
 
 ?>
-
-
-
-
 <!-- Sign-up connection -->
 <?php
 if (isset($_POST['sign-up'])) {
@@ -82,7 +84,7 @@ if (isset($_POST['sign-up'])) {
   $username = mysqli_real_escape_string($conn, $_POST['email']);
   $phonenumber = mysqli_real_escape_string($conn, $_POST['phone_number']);
   $password = mysqli_real_escape_string($conn, $_POST['password']);
-  $Confirm_password = mysqli_real_escape_string($conn, $_POST['confPassword']);   
+  $Confirm_password = mysqli_real_escape_string($conn, $_POST['confPassword']);
 
   // form validation: ensure that the form is correctly filled ...
   // by adding (array_push()) corresponding error unto $errors array
@@ -95,12 +97,12 @@ if (isset($_POST['sign-up'])) {
 	array_push($errors, "The confirmation passwords do not match your password");
   }
 
-  // first check the database to make sure 
+  // first check the database to make sure
   // a user does not already exist with the same username and/or email
   $user_check_query = "SELECT * FROM users WHERE email='$username' OR phone_number='$phonenumber' LIMIT 1";
   $result = mysqli_query($conn, $user_check_query);
   $user = mysqli_fetch_assoc($result);
-  
+
   if ($user) { // if user exists
     if ($user['email'] === $username) {
       array_push($errors, "email already exists");
@@ -113,16 +115,42 @@ if (isset($_POST['sign-up'])) {
 
   // Finally, register user if there are no errors in the form
   if (count($errors) == 0) {
-  	$password = md5($password);//encrypt the password before saving in the database
+      $password = md5($password); //encrypt the password before saving in the database
 
-  	$query = "INSERT INTO users (email, password, first_name, surname, phone_number) 
-  			  VALUES('$username', '$password', '$firstname', '$surname', '$phonenumber')";
-  	mysqli_query($conn, $query);
-  	$_SESSION['email'] = $username;
-    $_SESSION['user_id'] = $uid;
-  	$_SESSION['success'] = "You are now logged in";
-  	header('location: loginpage.php');
+      $query = "INSERT INTO users (email, password, first_name, surname, phone_number)
+              VALUES('$username', '$password', '$firstname', '$surname', '$phonenumber')";
+      mysqli_query($conn, $query);
+
+      // Get the last inserted user ID
+      $userID = mysqli_insert_id($conn);
+
+
+      // Insert address details into the address table
+      $street = mysqli_real_escape_string($conn, $_POST['street']);
+      $city = mysqli_real_escape_string($conn, $_POST['city']);
+      $state = mysqli_real_escape_string($conn, $_POST['state']);
+      $zip_code = mysqli_real_escape_string($conn, $_POST['zip_code']);
+
+
+      $query2 = "INSERT INTO address (user_id, street_address, city, state, postal_code)
+                      VALUES ('$userID', '$street', '$city', '$state', '$zip_code')";
+      mysqli_query($conn, $query2);
+
+
+
+      $_SESSION['email'] = $username;
+      $_SESSION['user_id'] = $userID; // Update $_SESSION['user_id']
+      $_SESSION['success'] = "You are now logged in";
+
+      // Log activity to the database, give date and time as well as action.
+      $timestamp = date('Y-m-d H:i:s');
+      $action = "registered";
+      $insert_query = "INSERT INTO user_logs (user_id, timestamp, action) VALUES ('$userID', '$timestamp', '$action')";
+      mysqli_query($conn, $insert_query);
+
+      header('location: loginpage.php');
   }
+
 }
 ?>
 
